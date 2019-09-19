2d6cfdc56f5572e65fc6b4d4c68ab12fd82004c2
Alexandre de Souza <alexandre@aledsz.com.br>
2019-09-19 02:20:10 -0300

    Added function to retrieve partner id from token

--- lib/paggi.ex
+++ lib/paggi.ex
@@ -14,10 +14,22 @@ defmodule Paggi do
 
   Our documentation can be accessed by: https://developers.paggi.com/reference
   """
+  alias JOSE.JWT
+
   @doc """
   Retrieve environment variables from configuration file
   """
   @spec get_env(binary | {:system, binary}) :: nil | binary
   def get_env(var) when is_binary(var), do: var
   def get_env({:system, var}) when is_binary(var), do: System.get_env(var)
+
+  @doc """
+  Retrieve partner_id from token
+  """
+  def get_partner_id() do
+    with {:ok, [token: token]} <- Application.fetch_env(:paggi, Paggi) do
+      %{"partner_id" => partner_id} = JWT.peek_payload(Paggi.get_env(token)).fields["permissions"]
+      partner_id
+    end
+  end
 end
