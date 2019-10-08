ffaecf4fee8e7b4b13b5b18facc4eef0a9f65557
Alexandre de Souza <alexandre@aledsz.com.br>
2019-10-08 00:37:26 -0300

    Added sub structs for Orders struct

--- lib/paggi/structs/orders.ex
+++ lib/paggi/structs/orders.ex
@@ -10,3 +10,39 @@ defmodule Paggi.Structs.Orders do
     :updated_at
   ]
 end
+
+defmodule Paggi.Structs.Orders.Recipients do
+  defstruct [
+    :id,
+    :document,
+    :name,
+    :amount
+  ]
+end
+
+defmodule Paggi.Structs.Orders.Customer do
+  defstruct [
+    :document,
+    :email,
+    :name
+  ]
+end
+
+defmodule Paggi.Structs.Orders.Charges do
+  defstruct [
+    :id,
+    :amount,
+    :card_id,
+    :installments,
+    :statuses
+  ]
+end
+
+defmodule Paggi.Structs.Orders.Charges.Statuses do
+  defstruct [
+    :amount,
+    :inserted_at,
+    :reason,
+    :type
+  ]
+end
