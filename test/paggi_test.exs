b5139daa66a59f934fa74285c7133a7b82b229cb
Alexandre de Souza <alexandre@aledsz.com.br>
2019-11-05 16:53:07 -0300

    Added test for ETS environment variable

--- test/paggi_test.exs
+++ test/paggi_test.exs
@@ -2,12 +2,21 @@ defmodule PaggiTest do
   use ExUnit.Case
   doctest Paggi
 
+  setup do
+    Paggi.init_ets()
+    Paggi.configure("PAGGI_VERSION", "v1")
+  end
+
   describe "Lib initialized" do
     test "so can retrieve environment variabe" do
       assert Paggi.get_env({:system, "PAGGI_ENVIRONMENT"}) == System.get_env("PAGGI_ENVIRONMENT")
     end
 
-    test "so Paggi can retrieve partner_id" do
+    test "so can retrieve environment variabe from ETS" do
+      assert Paggi.get_env({:ets, "PAGGI_VERSION"}) == System.get_env("PAGGI_VERSION")
+    end
+
+    test "so can retrieve partner_id" do
       assert not is_nil(Paggi.get_partner_id())
     end
   end
